ba4b0a55f8a07606f68851c3014cb58506dc33f5
xjwnb <1565163445@qq.com>
2020-03-03 12:00:50 +0800

    npm安装和简单配置element-UI

---
+++ src/components/NavMenu.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="NavMenu">
+    <el-row class="tac">
+      <el-col :span="12">
+    <h5>自定义颜色</h5>
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>导航一</span>
+        </template>
+        <el-menu-item-group>
+          <template slot="title">分组一</template>
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+        </el-menu-item-group>
+    </el-menu>
+  </el-col>
+</el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavMenu'
+}
+</script>
+
+<style scoped>
+</style>
