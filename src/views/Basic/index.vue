d2d8e33b2479915ef377a6aaa709ab191442ba3f
xjwnb <1565163445@qq.com>
2020-03-07 14:42:04 +0800

    Basic系列与Honme分离

---
+++ src/views/Basic/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="basic">
+    <Container></Container>
+  </div>
+</template>
+
+<script>
+import Container from 'components/Container/Container'
+
+export default {
+  name: 'Basic',
+  components: {
+    Container
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
