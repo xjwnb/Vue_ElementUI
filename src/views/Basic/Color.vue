377a17f18a3c3ba817144f0f8e394c49152340d3
xjwnb <1565163445@qq.com>
2020-03-07 23:19:46 +0800

    新建Color颜色页面

---
+++ src/views/Basic/Color.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="color">
+    <RectColor></RectColor>
+    <h1>哈哈哈</h1>    
+  </div>
+</template>
+
+<script>
+import RectColor from 'components/RectColor/RectColor'
+
+export default {
+  name: 'Color',
+  components: {
+    RectColor
+  }
+}
+</script>
+
