fa6290f39da6c3eb31de195cef25fc4b2fa8f056
xjwnb <1565163445@qq.com>
2020-03-07 23:18:56 +0800

    新建长方形颜色组件

---
+++ src/components/RectColor/RectColor.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="rect-color">
+    <div class="rect" @click="ClickHandle"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RectColor",
+  methods: {
+    ClickHandle() {
+      console.log('点击了长方形')
+    }
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.rect {
+  width: 66px;
+  height: 33px;
+  border: 1px solid greenyellow;
+  cursor: pointer;
+}
+</style>
