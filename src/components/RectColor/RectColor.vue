f4c200939b878ed11ab22cc4a71349b90c0fa45b
xjwnb <1565163445@qq.com>
2020-03-08 17:36:55 +0800

    发现Bug(Notification重叠问题

--- src/components/RectColor/RectColor.vue
+++ src/components/RectColor/RectColor.vue
@@ -1,25 +1,65 @@
 <template>
   <div id="rect-color">
-    <div class="rect" @click="ClickHandle"></div>
+    <el-button
+      class="btn"
+      plain
+      :style="{backgroundColor: color, border: borderColor}"
+      @click="ClickHandle"
+      data-clipboard-action="copy"
+      :data-clipboard-text="color"
+    >
+      <!-- <div class="rect" :style="{backgroundColor: color, border: borderColor}" @click="ClickHandle"></div> -->
+    </el-button>
   </div>
 </template>
 
 <script>
 export default {
   name: "RectColor",
-  methods: {
-    ClickHandle() {
-      console.log('点击了长方形')
+  props: {
+    color: {
+      type: String
+    },
+    borderColor: {
+      type: String
     }
   },
+  data() {
+    return {
+      notifyPromise: Promise.resolve()
+    };
+  },
+  methods: {
+     ClickHandle() {
+      let clipboard = new this.clipboard(".btn");
+      console.log("哈哈哈..");
+this.notifyPromise = this.notifyPromise.then(() => {
+        clipboard.on("success", () => {
+          console.log("hhh");
+
+           this.$notify({
+            message: "你所选中的颜色已经被复制到粘贴板上",
+            position: "bottom-right"
+          });
+        });
+        })
+
+      console.log("哈哈哈");
+
+      /*       clipboard.on("err", () => {
+        this.$notify({
+          message: "复制失败",
+          position: "bottom-right"
+        });
+      }); */
+    }
+  }
 };
 </script>
 
 <style lang="less" scoped>
-.rect {
+.btn {
   width: 66px;
   height: 33px;
-  border: 1px solid greenyellow;
-  cursor: pointer;
 }
 </style>
