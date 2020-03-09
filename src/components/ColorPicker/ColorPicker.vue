96e912eb7a160c6c2e66f7a4cbb925212c943ab2
xjwnb <1565163445@qq.com>
2020-03-09 18:44:24 +0800

    添加colorPicker可复用组件

---
+++ src/components/ColorPicker/ColorPicker.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <el-color-picker v-model="color2" :size="size"></el-color-picker>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ColorPicker",
+  props: {
+    color1: {
+      type: String,
+      default: '#00FF00'
+    },
+    size: {
+      type: String
+    },
+    id: {
+      type: Number
+    }
+  },
+  data() {
+    return {
+      color2: null
+    }
+  },
+  watch: {
+    color2() {
+      let arr = [this.color2, this.id]
+      this.$emit('colorChange', arr)
+    }
+  },
+  created() {
+    this.color2 = this.color1
+  },
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
