231e1f7a6937482fad22039c44d39bbf86b1d03f
xjwnb <1565163445@qq.com>
2020-03-09 18:46:16 +0800

    创建并引入相应组件,colorPicker页面暂时完成

---
+++ src/views/Basic/ColorPicker.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ColorPickerRect></ColorPickerRect>
+    <SizeColorPicker></SizeColorPicker>
+    <VColorPickerRect></VColorPickerRect>
+  </div>
+</template>
+
+<script>
+import ColorPickerRect from 'components/Basic/ColorPicker/ColorPickerRect'
+import SizeColorPicker from 'components/Basic/ColorPicker/SizeColorPicker'
+import VColorPickerRect from 'components/Basic/ColorPicker/VColorPickerRect'
+
+export default {
+  name: "ColorPicker",
+  components: {
+    ColorPickerRect,
+    SizeColorPicker,
+    VColorPickerRect
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
