4d4aba5bc71c71ab2b774a9299dce7912247ad23
xjwnb <1565163445@qq.com>
2020-03-09 18:45:06 +0800

    添加可复用vColorPicker组件

---
+++ src/components/vColorPicker/VColorPicker.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <colorPicker v-model="color2"></colorPicker>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VColorPicker",
+  props: {
+    color1: {
+      type: String
+    },
+    id: {
+      type: Number
+    }
+  },
+  data() {
+    return {
+      // color: "#00FF00"
+      color2: null
+    }
+  },
+  watch: {
+    color2() {
+      let arr = [this.id, this.color2]
+      this.$emit('colorChange', arr)
+    }
+  },
+  created() {
+    this.color2 = this.color1
+  },
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
