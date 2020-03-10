7837def5d4691c345355db1992afd2047bf8deda
xjwnb <1565163445@qq.com>
2020-03-10 21:02:26 +0800

    添加 Icon 可复用组件

---
+++ src/components/Icon/Icon.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="content" :style="{fontSize: size}">
+    <i class="icon" :class="class1" :style="{lineHeight: size}"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Icon',
+  props: {
+    class1: {
+      type: String
+    },
+    size: {
+      type: String,
+      default: '30px'
+    }
+  },
+  data() {
+    return {
+      green: 'yellowgreen'
+    }
+  },
+}
+</script>
+
+<style lang="less">
+.content {
+  margin: 10px 0px 10px;
+}
+</style>
