75f07a03bfe4d2ded8eb26406933456c617dc8b5
xjwnb <1565163445@qq.com>
2020-03-10 21:01:52 +0800

    添加 Icon 页面 的第一部分内容

---
+++ src/components/Basic/IconComs/IconComs.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="iconComs">
+    <Box class="box">
+      <h3>Icon 图标</h3>
+      <Slider :title="title" @sliderChange="sliderChange"></Slider>
+      <div class="content" :key="item.id" v-for="item in icons">
+        <Ic :class="item.iconClass" :size="sizeNew+'px'"></Ic>
+      </div>
+    </Box>
+  </div>
+</template>
+
+<script>
+import Box from "components/Box/Box";
+import Slider from 'components/Slider/Slider'
+import Ic from "components/Icon/Icon";
+
+import { icons } from 'util/const'
+
+export default {
+  name: "IconComs",
+  components: {
+    Box,
+    Slider,
+    Ic
+  },
+  data() {
+    return {
+      title: '改变 Icon 大小',
+      icons: null,
+      valueNew: null,
+      sizeNew: null
+    };
+  },
+  created() {
+    this.icons = icons
+  },
+  methods: {
+    sliderChange(value) {
+      this.valueNew = value
+      this.sizeNew = 30 + this.valueNew
+    }
+  },
+};
+</script>
+
+<style lang="less" scoped>
+h3 {
+  margin-bottom: 10px;
+}
+.box {
+  width: 100%;
+}
+
+.iconComs {
+  display: flex;
+  flex-wrap: wrap;
+  .content {
+    display: inline-block;
+    width: 25%;
+    text-align: center;
+  }
+}
+</style>
