f375fa7713f7c533b24d94d95bdbb79d0e469508
xjwnb <1565163445@qq.com>
2020-03-10 21:00:26 +0800

    添加 icons 数据

--- src/util/const.js
+++ src/util/const.js
@@ -29,6 +29,58 @@ const options = [
   }
 ]
 
+const icons = [
+  {
+    id: 1,
+    iconClass: "el-icon-edit"
+  },
+  {
+    id: 2,
+    iconClass: "el-icon-star-on"
+  },
+  {
+    id: 3,
+    iconClass: "el-icon-upload"
+  },
+  {
+    id: 4,
+    iconClass: "el-icon-phone"
+  },
+  {
+    id: 5,
+    iconClass: "el-icon-s-goods"
+  },
+  {
+    id: 6,
+    iconClass: "el-icon-video-camera-solid"
+  },
+  {
+    id: 7,
+    iconClass: "el-icon-s-help"
+  },
+  {
+    id: 8,
+    iconClass: "el-icon-help"
+  },
+  {
+    id: 9,
+    iconClass: "el-icon-minus"
+  },
+  {
+    id: 10,
+    iconClass: "el-icon-plus"
+  },
+  {
+    id: 11,
+    iconClass: "el-icon-check"
+  },
+  {
+    id: 12,
+    iconClass: "el-icon-close"
+  }
+]
+
 export {
-  options
+  options,
+  icons
 }
