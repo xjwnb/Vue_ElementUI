6aadb1e290cabb21db5fd2cc0fdf3f3485162262
xjwnb <1565163445@qq.com>
2020-03-10 21:02:56 +0800

    添加 Slider 可复用组件

---
+++ src/components/Slider/Slider.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="slider">
+    <div class="block">
+      <span class="demonstration">{{title}}</span>
+      <el-slider v-model="value1" @input="sliderHandle(value1)"></el-slider>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Slider",
+  props: {
+    title: {
+      type: String
+    },
+    min: {
+      type: Number
+    },
+    max: {
+      type: Number
+    }
+  },
+  data() {
+    return {
+      value1: 24
+    }
+  },
+  activated() {
+    this.value1 = 24
+  },
+  methods: {
+    sliderHandle(value) {
+      this.$emit('sliderChange', value)
+    }
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.block {
+  width: 50%;
+}
+</style>
