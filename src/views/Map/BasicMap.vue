e684fe690c0cf4afd232648ba31baae99a4924ed
xjwnb <1565163445@qq.com>
2020-03-13 22:34:44 +0800

    新建 Map 页面

---
+++ src/views/Map/BasicMap.vue
@@ -0,0 +1,70 @@
+<template>
+  <baidu-map
+    class="map"
+    :center="center"
+    :zoom="zoom"
+    @ready="handler"
+    style="height:1080px"
+    @click="getClickInfo"
+    :scroll-wheel-zoom="true"
+  ></baidu-map>
+</template>
+
+<script>
+export default {
+  name: "BasicMap",
+
+  data() {
+    return {
+      center: { lng: 116.4, lat: 39.915 },
+      zoom: 13
+    };
+  },
+  methods: {
+    handler({ BMap, map }) {
+      var point = new BMap.Point(116.4, 39.915);
+      map.centerAndZoom(point, 13);
+      var marker = new BMap.Marker(point);
+      map.addOverlay(marker);
+      var circle = new BMap.Circle(point, 6, {
+        strokeColor: "Red",
+        strokeWeight: 6,
+        strokeOpacity: 1,
+        Color: "Red",
+        fillColor: "#f03"
+      });
+      map.addOverlay(circle);
+    },
+    getClickInfo(e) {
+      this.center.lng = e.point.lng;
+      this.center.lat = e.point.lat;
+    }
+  }
+};
+</script>
+
+
+
+<style lang="less">
+.el-main {
+  width: 100%;
+  height: 50%;
+  overflow: hidden;
+  .map {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }
+}
+.BMap_mask {
+  width: 100%;
+  height: 50%;
+}
+</style> 
+
+<style lang="less" scoped>
+/* .map {
+  width: 100%;
+  height: 50%;
+} */
+</style> 
---
+++ src/views/Map/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="map">
+    <Container></Container>
+  </div>
+</template>
+
+<script>
+import Container from 'components/Container/Container'
+
+export default {
+  name: 'Map',
+  components: {
+    Container
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
